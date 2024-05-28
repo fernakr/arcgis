d108e4dfbb8f3ded2ea34d5f1ad6a44fc356828d
Kristine Fernandez <kfernandez@KristinandezMBP.lan>
2024-05-28 11:49:49 -0500

    zoom to multiple points

--- esm-samples/jsapi-react/src/App.jsx
+++ esm-samples/jsapi-react/src/App.jsx
@@ -5,6 +5,9 @@ import MapView from "@arcgis/core/views/MapView";
 import WebMap from "@arcgis/core/WebMap";
 import GraphicsLayer from '@arcgis/core/layers/GraphicsLayer';
 import Graphic from '@arcgis/core/Graphic';
+// import Point from '@arcgis/core/geometry/Point';
+// import Extent from '@arcgis/core/geometry/Extent';
+// import BasemapToggle from '@arcgis/core/widgets/BasemapToggle';
 // import PictureMarkerSymbol from '@arcgis/core/symbols/PictureMarkerSymbol';
 // import mapImage from './map.jpg';
 // import * as geodesicUtils from "@arcgis/core/geometry/support/geodesicUtils.js";
@@ -32,22 +35,17 @@ function App() {
   const [filterValue, setFilterValue] = React.useState(false);
   const [gravesView, setGravesView] = React.useState(null);
   const mapDiv = useRef(null);
+
   
-  let graveLayer, activeLayer;
-  let map;
-  //let filterExpression = null;
-
-  /* The commented out `updateFilter` function is intended to toggle the visibility of a layer on the
-  map based on a filter condition. Here's a breakdown of what the function does: */
+  let graveLayer;
   
-
-
+  
 
   useEffect(() => {
    // Create a map and view
    esriConfig.apiKey = "AAPK1603870d3538472fb7f6c4d1accdec01rx8QfJvj3jb-IW_qiJR_dYdKvNMFUSvt1HQOWd7OjMFMiuUtCVBmUwyLHuDAxRkx";
    
-   map = new WebMap({
+    const map = new WebMap({
       portalItem: {
         id: 'b8fbdd2f710e4414844565206609892e'
       },
@@ -75,9 +73,13 @@ function App() {
       zoom: 17, // Set your desired zoom level
     });
 
-    
     const userPositionLayer = new GraphicsLayer();
     map.add(userPositionLayer);
+
+    mapDiv.view = view;
+    mapDiv.map = map;
+    mapDiv.userPositionLayer = userPositionLayer;
+   
 
     view.when(() => {
 
@@ -89,6 +91,7 @@ function App() {
         return layer instanceof FeatureLayer;
       });
 
+      let legendValues = [];
       featureLayers.forEach(function(featureLayer) {
         //console.log('featureLayer', featureLayer.title);
         if (featureLayer.title.includes('Graves')) {
@@ -118,33 +121,14 @@ function App() {
             ]
           });
 
+          legendValues.push(legendValues.concat([{
+            layer: featureLayer,
+            title: "Legend"
+          }]));
+
           // Set the renderer to the feature layer
           featureLayer.renderer = renderer;
-          var legend = new Legend({
-            view: view,
-            layerInfos: [{
-              layer: featureLayer,
-              title: "Legend"
-            }]
-          });
-        
-          const fullscreen = new Fullscreen({
-            label: 'Expand',
-            view: view
-          });
-          
-          const compass = new Compass({
-            view: view
-          });
-
-          // min/max zoom
-          view.constraints.minZoom = 16;
-          view.constraints.maxZoom = 20;
-
-          view.ui.add(compass, "top-left");
-          view.ui.add(fullscreen, "top-right");
-          // Add the legend to the view
-          view.ui.add(legend, "bottom-right");
+         
         
           view.whenLayerView(graveLayer).then((layerView) => {
             // flash flood warnings layer loaded
@@ -185,12 +169,41 @@ function App() {
             }]
           }                    
 
+        }else{
+          legendValues.push(legendValues.concat([{
+            layer: featureLayer,
+            title: "Legend"
+          }]));
         }
         
 
         
       });
       
+      const fullscreen = new Fullscreen({
+        label: 'Expand',
+        view: view
+      });
+      
+      const compass = new Compass({
+        view: view
+      });
+
+      var legend = new Legend({
+        view: view,
+        layerInfos: legendValues
+      });
+
+      // min/max zoom
+      // view.constraints.minZoom = 16;
+      // view.constraints.maxZoom = 20;
+
+      view.ui.add(compass, "top-left");
+      view.ui.add(fullscreen, "top-right");
+      // Add the legend to the view
+
+      view.ui.add(legend, "bottom-right");
+    
 
       
 
@@ -208,63 +221,31 @@ function App() {
     });
 
 
-    // Function to add a marker for the browser's GPS location
-    const addGPSMarker = (position) => {
-      // Clear previous GPS marker
-      userPositionLayer.removeAll();
 
-      // create a marker with a person icon
-
-
-
-
-      const marker = new Graphic({
-        geometry: {
-          type: 'point',
-          x: position.coords.longitude,
-          y: position.coords.latitude,
-        },
-        symbol: {
-        type: "text", // autocasts as new TextSymbol()
-        color: "#ebab34",
-        text: "\ue675", // esri-icon-user
-        font: {
-          // autocasts as new Font()
-          size: 36,
-          family: "CalciteWebCoreIcons" // Esri Icon Font
-        }
-      },
-      });
-
-      userPositionLayer.add(marker);
-
-      // center map on user location
-      //view.center = [position.coords.longitude, position.coords.latitude];
-      
-    };
 
     // Use the browser's Geolocation API to continuously watch for changes in position
-    const watchId = navigator.geolocation.watchPosition(
-      (position) => {
-        // Add a marker for the browser's GPS location
-        addGPSMarker(position);
+    // const watchId = navigator.geolocation.watchPosition(
+    //   (position) => {
+    //     // Add a marker for the browser's GPS location
+    //     addGPSMarker(position);
         
   
-        // Center the view on the updated GPS location
-        //view.center = [position.coords.longitude, position.coords.latitude];
-      },
-      (error) => {
-        console.error('Error getting GPS location:', error.message);
-      }
-    );
+    //     // Center the view on the updated GPS location
+    //     //view.center = [position.coords.longitude, position.coords.latitude];
+    //   },
+    //   (error) => {
+    //     console.error('Error getting GPS location:', error.message);
+    //   }
+    // );
   
     // Clean up the watchPosition when the component is unmounted
     return () => {
-      navigator.geolocation.clearWatch(watchId);
+      // navigator.geolocation.clearWatch(watchId);
     };
-    
+
     
   }, [mapDiv]);
+
 
   useEffect(() => {
     let filterExpression = filterValue ? "status = 'occupied'" : null;
@@ -277,11 +258,87 @@ function App() {
     }    
   }, [filterValue, gravesView]);
 
+  
+  const addGPSMarker = (position) => {
+
+  }
+
+
+  const findMe = () => {
+    mapDiv.view.when(() => {
+
+      navigator.geolocation.getCurrentPosition((position) => {
+        // Add a marker for the browser's GPS location
+         mapDiv.userPositionLayer.removeAll();
+
+        // create a marker with a person icon
+
+
+
+
+        // const marker = new Graphic({
+        //   geometry: {
+        //     type: 'point',
+        //     x: position.coords.longitude,
+        //     y: position.coords.latitude,
+        //   },
+        //   symbol: {
+        //   type: "text", // autocasts as new TextSymbol()
+        //   color: "#ebab34",
+        //   text: "\ue675", // esri-icon-user
+        //   font: {
+        //     // autocasts as new Font()
+        //     size: 36,
+        //     family: "CalciteWebCoreIcons" // Esri Icon Font
+        //   }
+        // },
+        // });
+
+        const userPosition = [position.coords.longitude, position.coords.latitude];
+        const points = [userPosition, [-97.726293, 30.266041]];
+
+        //console.log('points', points);
+        //mapDiv.userPositionLayer.add(marker);
+
+        const graphics = points.map((point) => {
+          return new Graphic({
+            geometry: { type: "point", x: point[0], y: point[1] },
+            symbol: {
+              type: 'simple-marker',
+              color: 'red',
+              size: '8px'
+            }
+          });
+        });
+  
+        mapDiv.view.graphics.addMany(graphics);
+        
+        
+
+        mapDiv.view.when(() => {
+          mapDiv.view.goTo({
+            target: graphics
+          });
+        });
+  
+
+  
+      });
+    });
+//    console.log(map);
+    //alert('find me');
+    //console.log(userPositionLayer);
+
+  }
+    
 
   return (<div className="wrapper">
     <h1>Search the Map</h1>
      <button onClick={ e => setFilterValue(!filterValue) }>Filter</button> 
-    
+    <div>
+      <button onClick={findMe}>Find Me</button>
+
+    </div>
     <div className="mapDiv" ref={mapDiv}></div>
   </div>)
 }
